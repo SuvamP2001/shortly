7e22d16c038074edb3f337827fa367c89fad125a
Prince Roy <princeku.ray143@gmail.com>
2024-05-09 14:16:43 +0530

    create and style navbar, header, and link-form

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HeaderComponent } from './header/header.component';
+import { MainSectionComponent } from './main-section/main-section.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    NavbarComponent,
+    HeaderComponent,
+    MainSectionComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'client';
